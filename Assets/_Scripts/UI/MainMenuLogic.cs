0f639cb6d621df8b993c580bafdc0d7fe814fceb
jskyzero <jskyzero@outlook.com>
2019-06-30 21:05:28 +0800

    :bento: add assets and many folders

---
+++ Assets/_Scripts/UI/MainMenuLogic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenuLogic : MonoBehaviour {
+  // Start is called before the first frame update
+  void Start() {
+
+  }
+
+  // Update is called once per frame
+  void Update() {
+
+  }
+
+  public void NaviToFirstScene() {
+    SceneManager.LoadScene("Scene0", LoadSceneMode.Single);
+  }
+}

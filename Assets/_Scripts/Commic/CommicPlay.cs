4a075061c1c9f51e4c9b919b5a49b574a0df2240
jskyzero <jskyzero@outlook.com>
2019-06-30 23:33:35 +0800

    :bento: add commit scene and chaper 0

---
+++ Assets/_Scripts/Commic/CommicPlay.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class CommicPlay : MonoBehaviour {
+
+  public GameObject[] movies;
+
+  private Vector3 targetPostion;
+
+  // Start is called before the first frame update
+  void Start() {
+    StartCoroutine(PlayVideo());
+  }
+
+  IEnumerator PlayVideo() {
+    foreach (var movie in movies) {
+      Debug.Log(movie.name);
+      var player = movie.GetComponent<VideoPlayer>();
+      player.Play();
+      yield return new WaitForSeconds(1);
+      player.Pause();
+
+    }
+  }
+
+  // Update is called once per frame
+  void Update() {
+
+  }
+}

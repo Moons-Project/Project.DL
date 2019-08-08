25ce476dcd199e17e1c3355c22002b606e6edbe8
jskzyero <jskyzero@outlook.com>
2019-08-08 10:29:22 +0800

    :bulb: begin comment source code

--- Assets/Editor/Builder.cs
+++ Assets/Editor/Builder.cs
@@ -1,13 +1,18 @@
-﻿using UnityEditor;
+﻿// 自動打包，腳本
+
+using UnityEditor;
+
 class Builder {
   static void build() {
 
     // Place all your scenes here
     string[] scenes = { "Assets/_Scenes/MainMenu.unity" };
-
+    // Deploy path
     string pathToDeploy = "D:\\Desktop\\output\\Project.DL.exe";
 
-    BuildPipeline.BuildPlayer(scenes, pathToDeploy, BuildTarget.StandaloneWindows, BuildOptions.None);
+    BuildPipeline.BuildPlayer(scenes, pathToDeploy,
+      BuildTarget.StandaloneWindows, BuildOptions.None);
+
     EditorApplication.Exit(0);
   }
 }

344c24519a0f0950b979f452157fa5b3a0788b13
jskyzero <jskyzero@outlook.com>
2019-09-01 17:28:31 +0800

    :bento: add new scene

---
+++ Assets/_Scripts/Chapter/4/System4.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Jsky.Manager;
+
+public class System4 : MonoBehaviour {
+  // Start is called before the first frame update
+  void Start() {
+    // UIManager.Instance.tipsLayer.ShowHintText("待续....", 10.0f, 0.0f);
+  }
+
+  // Update is called once per frame
+  void Update() {
+
+  }
+}
